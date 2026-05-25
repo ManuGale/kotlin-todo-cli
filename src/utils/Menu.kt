fb2609959c72258b42bba15c465a934f5f2f6cca
ManuGale <133926506+ManuGale@users.noreply.github.com>
2026-05-25 15:21:02 -0500

    Se implementó menú principal de la aplicación

---
+++ src/utils/Menu.kt
@@ -0,0 +1,17 @@
+package utils
+
+fun mostrarMenu() {
+
+    println(
+        """
+        =========================
+              TO-DO CLI
+        =========================
+        1. Agregar tarea
+        2. Listar tareas
+        3. Completar tarea
+        4. Eliminar tarea
+        5. Salir
+        """.trimIndent()
+    )
+}
