a9042cbee441d1c4705b9775fc865251eb852ae7
Raymundo0407 <l22530406@cancun.tecnm.mx>
2026-05-26 18:00:54 -0500

    Implementar guardado de tareas

--- src/services/ArchivoService.kt
+++ src/services/ArchivoService.kt
@@ -1,9 +1,25 @@
 package services
 
+import models.Tarea
 import java.io.File
 
 class ArchivoService {
 
     private val archivo = File("src/data/tareas.txt")
 
+    fun guardarTareas(tareas: List<Tarea>) {
+
+        archivo.printWriter().use { out ->
+
+            tareas.forEach { tarea ->
+
+                out.println(
+                    "${tarea.id}," +
+                    "${tarea.titulo}," +
+                    "${tarea.descripcion}," +
+                    "${tarea.completada}"
+                )
+            }
+        }
+    }
 }
