3ff7237e8a1ff119f04bb8b6dc82e520ef412a9c
Carlos <l21530240@cancun.tecnm.mx>
2026-05-26 17:51:38 -0500

    feat: implementar registro de tareas

--- src/services/TareaService.kt
+++ src/services/TareaService.kt
@@ -7,4 +7,23 @@ class TareaService {
     private val archivoService = ArchivoService()
 
     private val tareas = archivoService.cargarTareas()
+
+    fun agregarTarea(titulo: String, descripcion: String) {
+
+        val nuevoId =
+            if (tareas.isEmpty()) 1
+            else tareas.maxOf { it.id } + 1
+
+        val tarea = Tarea(
+            nuevoId,
+            titulo,
+            descripcion
+        )
+
+        tareas.add(tarea)
+
+        archivoService.guardarTareas(tareas)
+
+        println("Tarea agregada correctamente")
+    }
 }
